5e9abaa067ed07162c29dc5ef8cce58735ee068e
AsadulloAkramov <asadullo.2807@gmail.com>
2022-03-19 11:26:05 +0500

    event binding is added

--- src/app/lessons.component.ts
+++ src/app/lessons.component.ts
@@ -11,6 +11,7 @@ export class LessonsComponent {
     this.lessons = lessonsService.getLessons();
   }
   title:string = 'Darslar ro\'yxati';
+  userName: string = "";
   lessons!: Array<string>;
   logoUrl:string = 'https://t4.ftcdn.net/jpg/02/81/70/65/240_F_281706507_HFR2DYj1w8bYo6AlzgEtFGPRPegu2yu9.jpg';
 
@@ -18,5 +19,12 @@ export class LessonsComponent {
     return this.title;
   }
 
+  onInput(event:any){
+    this.userName = event.target.value
+    console.log(event);
+  }
+  onButtonClicked(){
+    console.log("Button bosildi");
+  }
  
 }
